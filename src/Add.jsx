d58453292f10fc9afba763098d75bcd83a20a028
FerrariRoma <85842907+wayneglik@users.noreply.github.com>
2022-07-14 00:35:00 +0900

    Update: Add page and Edit page

--- src/Add.jsx
+++ src/Add.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState } from "react";
 import { HeaderStyled, InputBodyStyled } from "./styleComponent";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 const Add = () => {
-  const nav = useNavigate();
+  const navigate = useNavigate();
 
   // 각각의 input에 대한 ref
   const wordRefInput = useRef(false);
@@ -14,6 +15,14 @@ const Add = () => {
 
   // 입력 필드가 다 채워지면 state변경
   const [isWritten, setIsWritten] = useState(false);
+
+  const createData = {
+    0: wordRefInput.current.value,
+    1: pronunciationRefInput.current.value,
+    2: meaningRefInput.current.value,
+    3: egRefInput.current.value,
+    4: translationRefInput.current.value,
+  };
 
   // 입력 필드가 비었을 때는 버튼 비활성화하기
   const InputOnChange = () => {
@@ -33,7 +42,8 @@ const Add = () => {
   // 입력 필드가 비었을 때 경고, 아니면 홈으로 보내기(나중에는 ADD액션함수 호출을 더할 예정)
   const btnOnClick = () => {
     if (isWritten) {
-      return nav("/");
+      console.log(createData);
+      return navigate("/");
     } else {
       return alert("입력 칸을 모두 채우세요.");
     }
--- src/Edit.jsx
+++ src/Edit.jsx
@@ -1,11 +1,16 @@
 import React, { useRef, useState } from "react";
 import { HeaderStyled, InputBodyStyled } from "./styleComponent";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { actionUpdateFn } from "./redux/modules/reduxWord";
 
-const Edit = ({ wordInfo }) => {
-  const nav = useNavigate();
+const Edit = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  console.log(wordInfo);
+  const { state } = useLocation();
+  const wordData = { ...state };
+
   // 각각의 input에 대한 ref
   const wordRefInput = useRef(false);
   const pronunciationRefInput = useRef(false);
@@ -16,26 +21,31 @@ const Edit = ({ wordInfo }) => {
   // 입력 필드가 다 채워지면 state변경
   const [isWritten, setIsWritten] = useState(false);
 
-  // 입력 필드가 비었을 때는 버튼 비활성화하기
-  const InputOnChange = () => {
-    if (
-      wordRefInput.current.value &&
-      pronunciationRefInput.current.value &&
-      meaningRefInput.current.value &&
-      egRefInput.current.value &&
-      translationRefInput.current.value
-    ) {
-      return setIsWritten(true);
-    } else {
-      return setIsWritten(false);
-    }
-  };
-
   // 입력 필드가 비었을 때 경고, 아니면 홈으로 보내기(나중에는 ADD액션함수 호출을 더할 예정)
   const btnOnClick = () => {
-    if (isWritten) {
-      return nav("/");
+    if (
+      !(
+        wordRefInput.current.value === "" ||
+        pronunciationRefInput.current.value === "" ||
+        meaningRefInput.current.value === "" ||
+        egRefInput.current.value === "" ||
+        translationRefInput.current.value === ""
+      )
+    ) {
+      const createData = {
+        0: wordRefInput.current.value,
+        1: pronunciationRefInput.current.value,
+        2: meaningRefInput.current.value,
+        3: egRefInput.current.value,
+        4: translationRefInput.current.value,
+        5: wordData.id,
+      };
+      dispatch(actionUpdateFn(createData));
+      setIsWritten(true);
+
+      return navigate("/");
     } else {
+      setIsWritten(false);
       return alert("입력 칸을 모두 채우세요.");
     }
   };
@@ -49,37 +59,37 @@ const Edit = ({ wordInfo }) => {
         <input
           type="text"
           placeholder="단어"
-          onChange={InputOnChange}
           ref={wordRefInput}
           name="word"
+          defaultValue={wordData[0]}
         />
         <input
           type="text"
           placeholder="발음"
-          onChange={InputOnChange}
           ref={pronunciationRefInput}
           name="pronunciation"
+          defaultValue={wordData[1]}
         />
         <input
           type="text"
           placeholder="뜻"
-          onChange={InputOnChange}
           ref={meaningRefInput}
           name="meaning"
+          defaultValue={wordData[2]}
         />
         <input
           type="text"
           placeholder="예문"
-          onChange={InputOnChange}
           ref={egRefInput}
           name="eg"
+          defaultValue={wordData[3]}
         />
         <input
           type="text"
           placeholder="예문해석"
-          onChange={InputOnChange}
           ref={translationRefInput}
           name="translation"
+          defaultValue={wordData[4]}
         />
 
         <button type="submit" onClick={btnOnClick}>
